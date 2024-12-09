6e2fb7f57d563d45ee6b7017351c7102ca086aa6
CChris152 <1650306859@qq.com>
2024-12-09 21:24:59 +0800

    add 项目环境和一个实例

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <div>
+    <button @click="fetchData">获取数据</button>
+    <ul v-if="users.length">
+      <li v-for="user in users" :key="user.id">{{ user.name }}</li>
+    </ul>
+    <p v-else>没有数据</p>
+  </div>
+</template>
+
+<script setup>
+import axios from 'axios';
+import { ref } from 'vue';
+
+const users = ref([]);
+
+const fetchData = async () => {
+  try {
+    const response = await axios.get('api/user/list');
+    console.log('请求成功');
+    users.value = response.data;
+    console.log(users);
+    console.log('请求成功');
+  } catch (err) {
+    console.error('请求失败:', err);
+  }
+};
+</script>
+
+<style>
+
+</style>
