f141b5fa6a5114a88f578fa6fd1cf4d15197ff0e
Lyunzh <yunzhi070@gmail.com>
2024-12-17 17:36:04 +0800

    forum init

--- src/router/index.js
+++ src/router/index.js
@@ -1,1 +1,66 @@
+import AdminCenterView from '@/views/AdminCenter/AdminCenterView.vue';
+import AdminLoginView from '@/views/AdminLogin/AdminLoginView.vue';
+import CategoryView from '@/views/Category/CategoryView.vue';
+import HomeView from '@/views/Home/HomeView.vue';
+import LayoutView from '@/views/Layout/LayoutView.vue';
+import PostView from '@/views/Post/PostView.vue';
+import UserCenterView from '@/views/UserCenter/UserCenterView.vue';
+import UserHomepageView from '@/views/UserHomepage/UserHomepageView.vue';
+import UserLoginView from '@/views/UserLogin/UserLoginView.vue';
+import { createRouter, createWebHistory } from 'vue-router';
+
+//页面路由配置
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'layout',
+      component: LayoutView,
+      children: [
+        {
+          path: '',
+          name: 'home',
+          component: HomeView
+        },
+        {
+          path: 'category/:id',
+          name: 'category',
+          component: CategoryView
+        },
+        {
+          path: 'post/:id',
+          name: 'post',
+          component: PostView
+        }
+      ]
+    },
+    {
+      path: '/usercenter',
+      name: 'usercenter',
+      component: UserCenterView,
+    },
+    {
+      path: '/userhomepage/:id',
+      name: 'userhomepage',
+      component: UserHomepageView,
+    },
+    {
+      path: '/userlogin',
+      name: 'userlogin',
+      component: UserLoginView,
+    },
+    {
+      path: '/adminlogin',
+      name: 'adminlogin',
+      component: AdminLoginView,
+    },
+    {
+      path: '/admincenter',
+      name: 'admincenter',
+      component: AdminCenterView,
+    }
+  ]
+});
  
+export default router;
